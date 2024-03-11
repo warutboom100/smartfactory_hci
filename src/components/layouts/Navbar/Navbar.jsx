1c3179174306cce0634519d00ace58d77a278d95
Iboomnarak <warutboom100@gmail.com>
2024-03-11 16:44:41 +0700

    test

--- src/components/layouts/Navbar/Navbar.jsx
+++ src/components/layouts/Navbar/Navbar.jsx
@@ -111,7 +111,7 @@ function Navbar({logindata}) {
           </ul>
         </li>
         
-        <li className="menu-button" onClick={showSidebar}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26"><path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg></a></li>
+        <li className="menu-button" onClick={showSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26"><path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg></a></li>
       </ul>
     </nav>
   );
